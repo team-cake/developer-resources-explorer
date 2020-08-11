cafd4e8d615469e5cad5208174a057283a2ec582
team-cake <eka.cheuk@gmail.com>
2020-08-11 16:41:07 +0200

    New feature added, user can select developer and show fav resources of said developer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import './App.css'
-import { selectDevelopersWithFavorite } from './store/developers/selectors'
+import {
+	selectDevelopersWithFavorite,
+	devFavResources,
+} from './store/developers/selectors'
 import { useSelector } from 'react-redux'
 
 const selectDevelopers = (reduxState) => {
@@ -15,10 +18,13 @@ export default function App() {
 	const developers = useSelector(selectDevelopers)
 	const resources = useSelector(selectResources)
 	const [favoriteId, setFavoriteId] = useState(1)
+	const [developerId, setDeveloperId] = useState(1)
 
 	const developersWithThisFavorite = useSelector(
 		selectDevelopersWithFavorite(favoriteId)
 	)
+
+	const favResources = useSelector(devFavResources(developerId))
 
 	return (
 		<div className='App'>
@@ -28,27 +34,54 @@ export default function App() {
 			<h2>{resources.length}</h2> Resources
 			<br />
 			<br />
-			<h2>
-				Who likes{' '}
-				<select
-					value={favoriteId}
-					onChange={(e) => setFavoriteId(parseInt(e.target.value))}
-				>
-					{resources.map((resource) => {
-						return (
-							<option key={resource.id} value={resource.id}>
-								{resource.name}
-							</option>
-						)
+			<div>
+				<h2>
+					Who likes{' '}
+					<select
+						value={favoriteId}
+						onChange={(e) => setFavoriteId(parseInt(e.target.value))}
+					>
+						{resources.map((resource) => {
+							return (
+								<option key={resource.id} value={resource.id}>
+									{resource.name}
+								</option>
+							)
+						})}
+					</select>
+					?
+				</h2>
+
+				<ul>
+					{developersWithThisFavorite.map((dev) => {
+						return <li key={dev.id}>{dev.name}</li>
 					})}
-				</select>
-				?
-			</h2>
-			<ul>
-				{developersWithThisFavorite.map((dev) => {
-					return <li key={dev.id}>{dev.name}</li>
-				})}
-			</ul>
+				</ul>
+			</div>{' '}
+			<br />
+			<div>
+				<h2>
+					What are{' '}
+					<select
+						value={developerId}
+						onChange={(e) => setDeveloperId(parseInt(e.target.value))}
+					>
+						{developers.map((developer) => {
+							return (
+								<option key={developer.id} value={developer.id}>
+									{developer.name}
+								</option>
+							)
+						})}
+					</select>
+					's favorites?
+				</h2>
+				<ul>
+					{favResources.map((resource) => {
+						return <li key={resource.id}>{resource.name}</li>
+					})}
+				</ul>
+			</div>
 		</div>
 	)
 }
