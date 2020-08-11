7a85c750b18fe3870d9261cb8edeec58bc896631
team-cake <eka.cheuk@gmail.com>
2020-08-11 15:34:08 +0200

    Added <select> field, user can pick resource to show the developers who favourite the resource.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { useSelector } from 'react-redux'
 
@@ -13,13 +13,40 @@ const selectResources = (reduxState) => {
 function App() {
 	const developers = useSelector(selectDevelopers)
 	const resources = useSelector(selectResources)
+	const [favId, setFavId] = useState(1)
 
+	const developersFav = useSelector((state) => {
+		return state.developers.filter((dev) => dev.favorites.includes(favId))
+	})
 	return (
 		<div className='App'>
 			<h1>Web development resources</h1>
 			<br />
 			<h2>{developers.length}</h2> Developers
 			<h2>{resources.length}</h2> Resources
+			<br />
+			<br />
+			<h2>
+				Who likes{' '}
+				<select
+					value={favId}
+					onChange={(e) => setFavId(parseInt(e.target.value))}
+				>
+					{resources.map((resource) => {
+						return (
+							<option key={resource.id} value={resource.id}>
+								{resource.name}
+							</option>
+						)
+					})}
+				</select>
+				?
+			</h2>
+			<ul>
+				{developersFav.map((dev) => {
+					return <li key={dev.id}>{dev.name}</li>
+				})}
+			</ul>
 		</div>
 	)
 }
